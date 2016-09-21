309879b68db711294fcecfb9ef46a65c8ab67b5f
yuttpan <yuttpanin@gmail.com>
2016-09-21 20:59:50 +0700

    a

---
+++ app.js
@@ -0,0 +1,21 @@
+
+    
+
+  var app = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
+
+
+/*
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+      .when('/', {
+        templateUrl: 'directives/home/home.html',
+        controller: 'HomeController'
+      })
+      .when('/LabPcu', {
+        templateUrl: 'pages/newLab/newLab.html',
+        controller: 'newLabController'
+      })
+
+      .otherwise({redirectTo: '/'});
+}]);
+*/
